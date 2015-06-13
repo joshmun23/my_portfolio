24499c4ac03e64b70a26fdcb4028c07b7f9d3c4f
Joshua Mun <joshmun23@gmail.com>
2015-06-13 16:45:25 -0400

    move profile image to bottom of featured, create side bar and add javascript for click and hover options

---
+++ app/assets/javascripts/sidebar.js
@@ -0,0 +1,29 @@
+$('#side-bar-arrow').on('click', function(e) {
+  e.preventDefault();
+
+  $(this).toggle('slide');
+  $('#side-bar').toggle('slide');
+  // $('#side-bar').addClass('show');
+});
+
+$('#side-bar').on('click', function(e) {
+  e.preventDefault();
+
+  $(this).toggle('slide');
+  $('#side-bar-arrow').toggle('slide');
+});
+
+$('#side-bar-arrow').on('mouseenter', function(e) {
+  e.preventDefault();
+
+  $(this).toggle('slide');
+  $('#side-bar').toggle('slide');
+  // $('#side-bar').addClass('show');
+});
+
+$('#side-bar').on('mouseleave', function(e) {
+  e.preventDefault();
+
+  $(this).toggle('slide');
+  $('#side-bar-arrow').toggle('slide');
+});
